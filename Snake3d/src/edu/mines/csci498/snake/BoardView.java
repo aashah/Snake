333b47b21154cfbfd3aa04fdd2b3ac5e684fc80c
Aakash Shah <ashah@mines.edu>
2012-12-02 18:33:43 -0700

    Added a BoardView to handle board-like view operations

---
+++ Snake3d/src/edu/mines/csci498/snake/BoardView.java
@@ -0,0 +1,18 @@
+package edu.mines.csci498.snake;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/*
+ * BoardView.java
+ * Description: Handles basic View model for Board-like displays. This incorporates handling
+ * 		tile arrays & their respective bitmaps.
+ */
+public class BoardView extends View {
+
+	public BoardView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+}
